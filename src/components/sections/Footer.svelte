3ce88dbc961b117a1f8e11e48df696ebdea76267
lepzulnag <gindre.matthieu@gmail.com>
2021-06-28 15:21:49 +0200

    ✨  Footer done

--- src/components/sections/Footer.svelte
+++ src/components/sections/Footer.svelte
@@ -15,7 +15,7 @@
 		<div class="infos">
 			{$_(`section.footer.signUp`)}
 		</div>
-		<button class="small">{$_(`subscribe`)}</button>
+		<button class="small margin-bottom-4">{$_(`subscribe`)}</button>
 		<div class="socialNetworks row">
 			{#each socialNetworks as [icon, href]}
 				<a {href}>
@@ -33,8 +33,8 @@
 		<div class="infos">
 			{$_(`section.footer.history`)}
 		</div>
-		<button class="small">{$_(`section.footer.discoverTheBrand`)}</button>
-		<div class="miscellaneous">
+		<button class="small margin-bottom-4">{$_(`section.footer.discoverTheBrand`)}</button>
+		<div class="miscellaneous row">
 			<a href="/">{$_(`section.footer.legalNotice`)}</a>
 			<a href="/">{$_(`section.footer.SDS`)}</a>
 			<a href="/">{$_(`section.footer.editSettings`)}</a>
@@ -53,13 +53,13 @@
 <style lang="sass">
 	footer
 		font-size: 3.5rem
-		justify-content: space-evenly
+		justify-content: space-around
 		color: var(--white)
-		padding: 12.5rem 0
+		padding: 12.5rem 5%
 
 		> *
 			max-width: 40%
-			gap: 5rem
+			gap: 4rem
 			align-items: start
 		a
 			color: var(--white)
@@ -68,11 +68,35 @@
 		button
 			border: 1px solid var(--white)
 	
+	aside
+		width: 65rem
+		
+	main
+		width: 115rem
+		
+	nav
+		align-self: center
+	
 	.socialNetworks
 		gap: 3rem
 	
 	.subtitle
 		flex-direction: row
-		gap: 7rem
+		gap: 5rem
+		font-size: 5rem
+		align-items: center
+		// margin-bottom: 1rem
+		font-weight: bold
+	
+	.slogan
+		font-size: 3rem
+	
+	.margin-bottom-4
+		margin-bottom: 4rem
+
+	.miscellaneous
+		gap: 7.5rem
+		font-weight: bold
+		font-size: 3rem
 
 </style>
